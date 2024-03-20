e1927abfa15da3da043e8942c46a7a287df9b93b
Denskerz <de.errr18@gmail.com>
2024-03-20 11:35:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,7 @@ import re
 import hashlib
 import logging
 import time
-import sys
 from re import Match
-from dotenv import load_dotenv
 from pathlib import Path
 
 
@@ -13,7 +11,7 @@ def time_ms():
     return round(time.time() * 1000)
 
 
-def hashing(line: str, personal_data: str) -> str | None:
+def hashing(line: str, personal_data: str) -> str:
     coincidence: Match[str] | None = re.search(personal_data, line)
     if coincidence is not None:
         tmp = hashlib.md5(str(coincidence).encode())
@@ -23,18 +21,19 @@ def hashing(line: str, personal_data: str) -> str | None:
 
 file_log = logging.FileHandler('Log.log')
 console_out = logging.StreamHandler()
-logging.basicConfig(level=logging.DEBUG, handlers=(file_log, console_out),
-                    format="[%(asctime)s | %(levelname)s]: %(message)s", datefmt='%m.%d.%Y ~ %H:%M:%S')
+logging.basicConfig(level=logging.DEBUG, handlers=(file_log, console_out), format="[%(asctime)s | %(levelname)s]: %(message)s", datefmt='%m.%d.%Y %H:%M:%S')
 
-ROOT_PATH = Path(__file__).resolve().parents[2]
+ROOT_PATH = Path(__file__).resolve().parents[0]
 DOTENV_PATH = os.path.join(ROOT_PATH, ".env")
-load_dotenv(dotenv_path=DOTENV_PATH)
 
-pattern_list: list[str] = os.getenv('ATTRIBUTES').split('\n')
-REGEXP = os.getenv('REGEXP')
+REGEXP_EXAMPLE='(?i)(?<=<EXAMPLE>).+?(?=</EXAMPLE>)|(?<=\"EXAMPLE\":\").+?(?=\")|(?i)(?<=<tns:EXAMPLE>).+?(?=</tns:EXAMPLE>)|(?<=\"tns:EXAMPLE\":\").+?(?=\")'
 
-for i in range(len(pattern_list)):
-    pattern_list[i] = re.sub(pattern='EXAMPLE', repl=pattern_list[i], string=REGEXP)
+ATTRIBUTES='CRMID,CRMApplicationID,ClientCRMID,LastName,FirstName,MiddleName,Email,ForProblZone,ReplacementFlag,BirthDate,Sex,Citizenship,RelationMilitary,EndDateDeferment,InfoResourcesAgree,InfoResourcesAgreeNumber,UNP,IBSO_ID,RegistrationNumber,RegistrationDate,StreetAddress,StreetAddressTypeCode,House,ApartmentNumber,Number,PartyDisplayName,PersonalNumber,IssueAuthority,IssueDate,ExpiryDate,City,Building,CategoryRelationship,CompanyNameFull'
+
+pattern_list: list = ATTRIBUTES.split(',')
+for pattern_number in range(len(pattern_list)):
+    pattern_list[pattern_number] = re.sub(pattern="(?i)EXAMPLE", repl=pattern_list[pattern_number], string=REGEXP_EXAMPLE)
 
 source = []
-listdir = os.listdir(f'{ROOT_PATH}/input_data')
+
+#listdir = os.listdir(f'{ROOT_PATH}/input_data')
